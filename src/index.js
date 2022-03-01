731e6237ea1c912f7732733ace1414e0b98f9e2c
Ahsan_4 <ahsanjawed2001@gmail.com>
2022-03-02 02:45:03 +0500

    bug fixing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Provider } from "./context/context";
 import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
